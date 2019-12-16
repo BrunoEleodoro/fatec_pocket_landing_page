d9a9f4d7d5dde7d5c689e889bd3e1f340a85a746
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-12-16 11:09:20 -0300

    Funcionalidades adicionadas

--- lib/jumbotron/right_image.dart
+++ lib/jumbotron/right_image.dart
@@ -23,11 +23,24 @@ class _RightImageState extends State<RightImage> {
     return AnimatedOpacity(
       duration: Duration(seconds: 2),
       opacity: show ? 1 : 0,
-      child: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('images/Login.png'))),
+      child: Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: 0.2,
+            child: Container(
+              width: double.maxFinite + 100,
+              height: double.maxFinite + 100,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.blueAccent),
+            ),
+          ),
+          Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('images/Login.png'))),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:ui';
+
 import 'package:fatec_pocket_landing_page/jumbotron/left_text.dart';
 import 'package:fatec_pocket_landing_page/jumbotron/right_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'funcionalidades/left_functions.dart';
+import 'funcionalidades/right_functions.dart';
 
 void main() => runApp(MyApp());
 
@@ -55,8 +60,54 @@ class _HomePageState extends State<HomePage> {
             ),
             Container(
               width: sizeWidth,
-              height: 300,
-              child: Text('fdsafda'),
+              height: 200,
+              alignment: Alignment.center,
+              child: Text('Funcionalidades',
+                  style: TextStyle(fontSize: 25, color: Colors.grey)),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              width: sizeWidth,
+              height: 600,
+              // color: Colors.blueAccent,
+              child: Stack(
+                children: <Widget>[
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: sizeWidth / 1.4,
+                      height: 350,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFF8e54e9)),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(flex: 3, child: LeftFunctions()),
+                      Expanded(
+                        flex: 4,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('images/ResumoV2.png'))),
+                        ),
+                      ),
+                      Expanded(flex: 3, child: RightFunctions()),
+                      SizedBox(
+                        width: 20,
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
