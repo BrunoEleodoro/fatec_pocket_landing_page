eb0352617045512f935daa2b9b34a226c1975a8e
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2020-04-12 15:33:15 +0000

    Updates

---
+++ precache-manifest.1b7a212cb173d5d2aa13d657c4e0db88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1819361b462c116d8b26f16f621b973d",
+    "url": "/fatec_pocket_landing_page/index.html"
+  },
+  {
+    "revision": "fd7ecbcbbcfd9d78a115",
+    "url": "/fatec_pocket_landing_page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7df698c39837241ca1f",
+    "url": "/fatec_pocket_landing_page/static/js/2.acd9b5e8.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/fatec_pocket_landing_page/static/js/2.acd9b5e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd7ecbcbbcfd9d78a115",
+    "url": "/fatec_pocket_landing_page/static/js/main.a87ea445.chunk.js"
+  },
+  {
+    "revision": "357d89738029195d3348",
+    "url": "/fatec_pocket_landing_page/static/js/runtime-main.8855170a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fatec_pocket_landing_page/static/media/logo.5d5d9eef.svg"
+  }
+]);
